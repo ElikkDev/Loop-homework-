ca53578847cd4e96c7649492bf381dc8f6a78f52
ElikkDev <eljanfn-bpa202@code.edu.az>
2025-10-07 12:48:02 +0400

    task is done

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Security.Cryptography;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Console.WriteLine("Enter  floor:");
+        //int floor = Convert.ToInt32(Console.ReadLine());
+
+        //int n = 0;
+
+        //while (floor > 0);
+            
+        //    n++
+        //    floor = floor//10 
+
+
+
+        //Console.WriteLine("Enter n :");
+        //int n = Convert.ToInt32(Console.ReadLine());
+        //int big = 0;
+
+        //for (int i = 1; i < n; i++)
+        //{
+        //       if (n % i ==0)
+        //        big = i;
+
+
+        //}
+
+        //Console.WriteLine("Big data: " + big);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //Console.WriteLine("Enter x :");
+        //int x = Convert.ToInt32(Console.ReadLine());
+
+        //Console.WriteLine("Enter y: ");
+        //int y = Convert.ToInt32(Console.ReadLine());
+
+        //if (y > x) Console.WriteLine("Wrong data");
+        //else
+        //{
+        //    int big = 0;
+
+        //    for (int i = 0; i < x; i++)
+        //    {
+        //        if (i % y == 0)
+        //            big = i;
+
+
+        //    }
+
+        //    Console.WriteLine("big dividor :" + big);
+        //}
+
+
+    }
+}
