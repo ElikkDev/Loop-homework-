983ee6f5b407edd0b926fb7fe67148da3d48a163
ElikkDev <eljanfn-bpa202@code.edu.az>
2025-10-07 13:30:09 +0400

    Third task

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,27 +6,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Enter x :");
-        int x = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter n :");
+        int n = Convert.ToInt32(Console.ReadLine());
+        int big = 0;
 
-        Console.WriteLine("Enter y: ");
-        int y = Convert.ToInt32(Console.ReadLine());
-
-        if (y > x) Console.WriteLine("Wrong data");
-        else
+        for (int i = 1; i < n; i++)
         {
-            int big = 0;
-
-            for (int i = 0; i < x; i++)
-            {
-                if (i % y == 0)
-                    big = i;
+            if (n % i == 0)
+                big = i;
 
 
-            }
-
-            Console.WriteLine("big dividor :" + big);
         }
+
+        Console.WriteLine("Big data: " + big);
     }
 
 
